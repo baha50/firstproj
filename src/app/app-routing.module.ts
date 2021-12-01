64fbc5218e8e919783ac54314dbc83ea9dd762ee
janhaoui baha <janhaouibaha@gmail.com>
2021-12-01 20:48:44 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { UserComponent } from './user/user.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+const routes: Routes = [
+  { path: "", component : HomepageComponent},
+  { path: "users", component : UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { HomepageComponent } from './homepage/homepage.component';
+<<<<<<< HEAD
 import { AboutComponent } from './about/about.component';
+=======
+import { NavbarComponent } from './navbar/navbar.component';
+>>>>>>> 07031ed (add routing)
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     HomepageComponent,
+<<<<<<< HEAD
     AboutComponent
+=======
+    NavbarComponent
+>>>>>>> 07031ed (add routing)
   ],
   imports: [
     BrowserModule,
